c85798843465d81f37ef71cdc86e5843425ba3b0
Ihar Lobach <lobachihor1994@gmail.com>
2020-07-30 23:32:33 -0500

    all random vals are doubel now, 3sigma uniform

--- coherent_modes.cpp
+++ coherent_modes.cpp
@@ -18,11 +18,13 @@ int nx, ny, nz;
 bool isinRange(int val, int r);
 std::complex<double> ex3d(const std::complex<double> *Ex3d, int z, int y, int x);
 bool isin(int a, int b, int c, int d, int l);
-std::complex<double> myexp(double xp, double yp, int m1, int n1, int m2, int n2, int il);
+std::complex<double> myexp(double xp, double yp, double fm1, double fn1, double fm2, double fn2, double lam);
 
 double sum(const std::complex<double> *Ex3d, int nx, int ny, int nz);
 
-double Sx,Sy,dx,dy,sxp,syp,dax,day,lmin,dl,sz,axmin,aymin,k0,re,im,V,mfold,xp,yp;
+double myrand(double LO, double HI);
+
+double Sx, Sy, dx, dy, sxp, syp, dax, day, lmin, lmax, dl, xmin, xmax, ymin, ymax, sz, k0, re, im, V, mfold, xp, yp, fm1, fn1, fm2, fn2, lam, xplim, yplim;
 int ixp, iyp, m1, n1, m2, n2, il;
 int m1Mixp, n1Miyp, m2Mixp, n2Miyp;
 int64_t m0;
@@ -52,14 +54,18 @@ int main(int argc, char* argv[])
     std::cout << "sxp = " << sxp << std::endl;
     syp = j["syp"];
     std::cout << "syp = " << syp << std::endl;
-    dax = j["dax"];
-    std::cout << "dax = " << dax << std::endl;
-    day = j["day"];
-    std::cout << "day = " << day << std::endl;
+    xmin = j["xmin"];
+    std::cout << "xmin = " << xmin << std::endl;
+    xmax = j["xmax"];
+    std::cout << "xmax = " << xmax << std::endl;
+    ymin = j["ymin"];
+    std::cout << "ymin = " << ymin << std::endl;
+    ymax = j["ymax"];
+    std::cout << "ymax = " << ymax << std::endl;
     lmin = j["lmin"];
     std::cout << "lmin = " << lmin << std::endl;
-    dl = j["dl"];
-    std::cout << "dl = " << dl << std::endl;
+    lmax = j["lmax"];
+    std::cout << "lmax = " << lmax << std::endl;
     sz = j["sz"];
     std::cout << "sz = " << sz << std::endl;
     m0 = j["m0"];
@@ -68,9 +74,6 @@ int main(int argc, char* argv[])
     std::cout << "mfold = " << mfold << std::endl;
 
     std::srand(seed);
-    std::default_random_engine generator;
-    std::normal_distribution<double> distx(0, sqrt(2) * sxp);
-    std::normal_distribution<double> disty(0, sqrt(2) * syp);
 
     auto data = xt::load_npy<std::complex<double>>(filepath);
     nx = data.shape()[2];
@@ -78,38 +81,59 @@ int main(int argc, char* argv[])
     ny = data.shape()[1];
     std::cout << "ny = " << ny << std::endl;
     nz = data.shape()[0];
+    dax = (xmax-xmin)/(nx-1);
+    day = (ymax-ymin)/(ny-1);
+    dl = (lmax-lmin)/(nz-1);
+    xplim = 3*sxp;
+    yplim = 3*syp;
     std::cout << "nz = " << nz << std::endl;
-    axmin = -dax * (nx - 1) / 2.0;
-    aymin = -day * (ny - 1) / 2.0;
     const std::complex<double> *Ex3d = data.data();
     double tot = dl * dax * day * sum(Ex3d, nx, ny, nz);
     std::cout << "tot = " << tot << std::endl;
-    V = dl * (nz - 1) * pow(dax * (nx - 1) * day * (ny - 1), 2);
+    V = dl * (nz - 1) * pow(dax * (nx - 1) * day * (ny - 1), 2) * (2*xplim*2*yplim);
     std::cout << "Starting the loop" << std::endl;
     s = 0;
     int64_t imax = m0*pow(2, mfold)+1;
     for (int64_t i = 1; i<imax; i++)
     { 
-        il = std::rand() % nz;
-        xp = distx(generator);
-        ixp = int(xp / dax + 0.5);
-        m1 = std::rand() % nx;
-        m2 = std::rand() % nx;
-        yp = disty(generator);
-        iyp = int(yp / day + 0.5);
-        n1 = std::rand() % ny;
-        n2 = std::rand() % ny;
-        m1Mixp = m1 - ixp;
-        n1Miyp = n1 - iyp;
-        m2Mixp = m2 - ixp;
-        n2Miyp = n2 - iyp;
+        lam = myrand(lmin, lmax);
+        fm1 = myrand(xmin, xmax);
+        fm2 = myrand(xmin, xmax);
+        fn1 = myrand(ymin, ymax);
+        fn2 = myrand(ymin, ymax);
+        m1 = int((fm1-xmin)/dax);
+        if (m1==nx){
+            m1 = nx-1;
+        }
+        m2 = int((fm2-xmin)/dax);
+        if (m2==nx){
+            m2 = nx-1;
+        }
+        n1 = int((fn1-ymin)/day);
+        if (n1==ny){
+            n1 = ny-1;
+        }
+        n2 = int((fn2-ymin)/day);
+        if (n2==ny){
+            n2=ny-1;
+        }
+        xp = myrand(-xplim, xplim);
+        yp = myrand(-yplim, yplim);
+        il = int((lam-lmin)/dl);
+        if (il==nz){
+            il=nz-1;
+        }
+        m1Mixp = int((fm1-xmin-xp)/dax+0.5);
+        n1Miyp = int((fn1-ymin-yp)/day+0.5);
+        m2Mixp = int((fm2-xmin-xp)/dax+0.5);
+        n2Miyp = int((fn2-ymin-yp)/day+0.5);
         if (isin(m1Mixp, n1Miyp, m2Mixp, n2Miyp, il))
         {
-            s = s * (double(i-1)/i) + (1.0/i) * pow(lmin+dl*il,2)*ex3d(Ex3d, il, n1Miyp, m1Mixp) * conj(ex3d(Ex3d, il, n1, m1)* ex3d(Ex3d, il, n2Miyp, m2Mixp)) * ex3d(Ex3d, il, n2, m2) * myexp(xp, yp, m1, n1, m2, n2, il);
+            s = s * (double(i-1)/i) + (1.0/i) * pow(lam,2)*ex3d(Ex3d, il, n1Miyp, m1Mixp) * conj(ex3d(Ex3d, il, n1, m1)* ex3d(Ex3d, il, n2Miyp, m2Mixp)) * ex3d(Ex3d, il, n2, m2) * myexp(xp, yp, fm1, fn1, fm2, fn2, lam);
         }
         if (i == m0)
         {
-            std::complex<double> M = pow(tot, 2) / (1.0/2.0/sqrt(M_PI) / sz * V * s);
+            std::complex<double> M = pow(tot, 2) / (1.0/2.0/sqrt(M_PI) / sz/4.0/M_PI/sxp/syp * V * s);
             std::time_t t = std::time(nullptr);
             std::cout << std::put_time(std::localtime(&t), "%c %Z") << std::endl;
             std::cout << "n points = " << i << ", M = " << M << std::endl;
@@ -163,11 +187,11 @@ bool isin(int a, int b, int c, int d, int l)
     return true;
 }
 
-std::complex<double> myexp(double xp, double yp, int m1, int n1, int m2, int n2, int il)
+std::complex<double> myexp(double xp, double yp, double fm1, double fn1, double fm2, double fn2, double lam)
 {
-    k0 = 2 * M_PI / (lmin + dl * il);
-    re = pow(k0 * Sx * dax * (m1 - m2), 2) + pow(k0 * Sy * day * (n1 - n2), 2);
-    im = k0 * dx * dax * (m1 - m2) * (dax * xp) + k0 * dy * day * (n1 - n2) * (day * yp);
+    k0 = 2 * M_PI / lam;
+    re = pow(xp/2/sxp,2) + pow(yp/2/syp,2) + pow(k0 * Sx * (fm1-fm2), 2) + pow(k0 * Sy * (fn1-fn2), 2);
+    im = k0 * dx * (fm1 - fm2) * xp + k0 * dy * (fn1 - fn2) * yp;
     std::complex<double> ar(re, im);
     return exp(-ar);
 }
@@ -186,4 +210,8 @@ double sum(const std::complex<double> *Ex3d, int nx, int ny, int nz)
         }
     }
     return res;
+}
+
+double myrand(double LO, double HI){
+    return LO + static_cast<double>(rand()) / (static_cast<double>(RAND_MAX / (HI - LO)));
 }
